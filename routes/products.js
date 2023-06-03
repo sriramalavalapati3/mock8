f910a67baad425b93f242655fdaf040b468ea1eb
sriram <sriramalavalapatiit01@gmail.com>
2023-06-03 15:46:49 +0530

    sort

--- routes/products.js
+++ routes/products.js
@@ -15,6 +15,35 @@ productrouter.post("/post",async(req,res)=>{
     }
 })
 
+productrouter.get("/post",async(req,res)=>{
+  
+    try {
+        let page=req.body||1
+        const perPage = 4; // Number of collections per page
+        const skip = (page - 1) * perPage;
+        let data= await Productmodel.find().skip(skip).limit(perPage);
+        
+        res.status(200).send(data)
+    } catch (error) {
+       res.send(error.message) 
+    }
+})
+
+productrouter.get("/sort/post",async(req,res)=>{
+  
+    try {
+        let data1=req.body;
+        let page=req.body||1
+        const perPage = 4; // Number of collections per page
+        const skip = (page - 1) * perPage;
+        let data= await Productmodel.find({category:data1}).skip(skip).limit(perPage);
+        
+        res.status(200).send(data)
+    } catch (error) {
+       res.send(error.message) 
+    }
+})
+
 
 
 
